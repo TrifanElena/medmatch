e1857e2a675ab0ad3f2684eb955b53ab472e7fdd
TrifanElena <trifanelenaiuliana29@gmail.com>
2025-05-13 01:58:31 +0300

    Serviciu de programare primitiv

--- appointments/models.py
+++ appointments/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.conf import settings
+from clinics.models import Clinic
 
-# Create your models here.
+class Appointment(models.Model):
+    patient = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    clinic = models.ForeignKey(Clinic, on_delete=models.CASCADE)
+    date = models.DateField()
+    time = models.TimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.patient.full_name} - {self.clinic.name} - {self.date} {self.time}"
--- appointments/views.py
+++ appointments/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import AppointmentForm
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+@login_required
+def create_appointment(request):
+    initial_data={}
+
+    clinic_id=request.GET.get('clinic_id')
+    if clinic_id:
+        initial_data['clinic'] = clinic_id
+
+    if request.method == 'POST':
+        form = AppointmentForm(request.POST)
+        if form.is_valid():
+            appointment = form.save(commit=False)
+            appointment.patient = request.user
+            appointment.save()
+            return redirect('services:home')  # sau o pagină de confirmare
+    else:
+        form = AppointmentForm(initial=initial_data)
+
+    return render(request, 'appointments/create_appointment.html', {'form': form})
